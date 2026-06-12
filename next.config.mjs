40693aa9e3dc0b74d823729abf4fa551c804a59f
04Shubhampatil <04Shubhampatil>
2026-06-12 15:57:35 +0530

    fix error
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: path.resolve(__dirname)
+  }
 };
 
 export default nextConfig;
